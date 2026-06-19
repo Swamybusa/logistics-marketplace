0d944dd163796aac73644dd8f49d8c2673d55722
Swamybusa <you@example.com>
2026-06-19 16:47:40 +0530

    feat: initialize Spring Boot project and create base package structure

---
+++ src/main/java/com/infotact/logistics_marketplace/LogisticsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.infotact.logistics_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotact/logistics_marketplace/LogisticsMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotact.logistics_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
